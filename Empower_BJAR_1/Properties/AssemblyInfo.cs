67d124fdbbc5b7bf81fd8cf137ddb18354ab825e
bjar <bjar@NOTE221059>
2023-03-28 14:11:54 +0200

    Add project files.

---
+++ Empower_BJAR_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_BJAR_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_BJAR_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F5634E32-27B7-4423-B860-B307A3F6E2B9")]
+[assembly: AssemblyVersion("1.0.0.0")]
